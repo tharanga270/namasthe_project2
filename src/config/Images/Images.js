3c73898b27cef90946dd85e6a66eb3d42da15bc3
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2023-12-17 18:35:25 +0530

    mui installed

---
+++ src/config/Images/Images.js
@@ -0,0 +1,62 @@
+import Team from "../../Pages/Team/Index";
+
+export const namastheLogo = require("../../Common/Assets/Images/NAMASTE_Curves-1 1.png");
+export const namastheLogo2 = require("../../Common/Assets/Images/NAMASTE_Curves-1 2.png");
+export const facebook = require("../../Common/Assets/Images/facebook.png");
+export const instagram = require("../../Common/Assets/Images/instagram.png");
+export const linkedin = require("../../Common/Assets/Images/linkedin.png");
+
+// Carousel
+export const Carousel1 = require("../../Common/Assets/Images/Property1.png");
+export const Carousel2 = require("../../Common/Assets/Images/Property2.png");
+export const Carousel3 = require("../../Common/Assets/Images/Property3.png");
+
+export const card1 = require("../../Common/Assets/Images/Copy of About us (6) 4.png");
+
+export const community1 = require("../../Common/Assets/Images/comminuty 1.png");
+export const detection1 = require("../../Common/Assets/Images/detection 1.png");
+export const engagement1 = require("../../Common/Assets/Images/engagement 1.png");
+export const intervention1 = require("../../Common/Assets/Images/intervention 1.png");
+
+export const group1 = require("../../Common/Assets/Images/Group.png");
+export const group2 = require("../../Common/Assets/Images/Group2.png");
+
+// Footer
+export const Rectangle = require("../../Common/Assets/Images/Rectangle__1_-removebg-preview.png");
+export const Rectangle1 = require("../../Common/Assets/Images/Rectangle__3_-removebg-preview.png");
+export const Rectangle2 = require("../../Common/Assets/Images/Rectangle__4_-removebg-preview.png");
+export const Rectangle3 = require("../../Common/Assets/Images/Rectangle__5_-removebg-preview.png");
+export const Rectangle4 = require("../../Common/Assets/Images/Rectangle-removebg-preview.png");
+
+export const projectcard1 = require("../../Common/Assets/Images/Clip path group.png");
+export const gif = require("../../Common/Assets/Images/Untitled design.gif");
+
+export const aboutus = require("../../Common/Assets/Images/Copy of About us (17) 1.png");
+export const aboutus1 = require("../../Common/Assets/Images/Copy of About us (17) 2.png");
+export const aboutus2 = require("../../Common/Assets/Images/Copy of About us (17) 6.png");
+export const email = require("../../Common/Assets/Images/email.png");
+
+export const projectcard2 = require("../../Common/Assets/Images/card.png");
+export const Team1 = require("../../Common/Assets/Images/team1.png");
+export const Team2 = require("../../Common/Assets/Images/team2.png");
+export const underconstruction = require("../../Common/Assets/Images/under-construction-2891888_1280.jpg");
+
+// SL Team
+export const asiri = require("../../Common/Assets/Images/asiri.png");
+export const raj = require("../../Common/Assets/Images/dr raj.png");
+export const chamika = require("../../Common/Assets/Images/chamika.png");
+export const hasuni = require("../../Common/Assets/Images/hasuni.png");
+export const heshali = require("../../Common/Assets/Images/heshali.png");
+export const poornima = require("../../Common/Assets/Images/madu.png");
+export const nethma = require("../../Common/Assets/Images/nethma.png");
+export const piumi = require("../../Common/Assets/Images/piyumi.png");
+export const rasika = require("../../Common/Assets/Images/rasika.png");
+export const sakuni = require("../../Common/Assets/Images/sakuni.png");
+export const shashini = require("../../Common/Assets/Images/professional photo.png");
+export const Shehara = require("../../Common/Assets/Images/shehara.png");
+export const yas = require("../../Common/Assets/Images/yashoda.png");
+export const abi = require("../../Common/Assets/Images/abi.png");
+export const venura = require("../../Common/Assets/Images/venura.png");
+
+export const laptopimg = require("../../Common/Assets/Images/About us-1.png");
+export const proobj = require("../../Common/Assets/Images/proobj.png");
