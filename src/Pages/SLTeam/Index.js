98d6534ccf79bbf34c21ad4a3550bf5510e83c18
tharanga270 <developerwordpress270@gmail.com>
2024-03-13 20:47:46 +0530

    3.images set

--- src/Pages/SLTeam/Index.js
+++ src/Pages/SLTeam/Index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import {
   email,
   asiri,
@@ -22,24 +22,28 @@ import {
   vidusha,
   dilini,
   ayendre,
-} from "../../config/Images/Images";
-import Tooltip from "@mui/material/Tooltip";
-import IconButton from "@mui/material/IconButton";
-import "./style.css";
+  jayamini,
+  madushalini,
+  anupi,
+  kushani,
+} from '../../config/Images/Images';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import './style.css';
 const data = [
   {
     id: 1,
     image: asiri,
-    name: "Dr. Asiri Hewamalage",
-    position: "Principle Investigator",
-    email: "asiri11@yahoo.com",
+    name: 'Dr. Asiri Hewamalage',
+    position: 'Principle Investigator',
+    email: 'asiri11@yahoo.com',
   },
   {
     id: 2,
     image: dilini,
-    name: "Dr. Dilini Vipulaguna",
-    position: "Principle Investigator",
-    email: "dilvip777@yahoo.com",
+    name: 'Dr. Dilini Vipulaguna',
+    position: 'Principle Investigator',
+    email: 'dilvip777@yahoo.com',
   },
   // {
   //   id: 3,
@@ -51,39 +55,39 @@ const data = [
   {
     id: 4,
     image: ayendre,
-    name: "Ayendree Senevirathne",
-    position: "Intervention Coordinator",
+    name: 'Ayendree Senevirathne',
+    position: 'Intervention Coordinator',
     // position2: "and Involement (CEI)",
-    email: "ayendree.namaste@gmail.com",
+    email: 'ayendree.namaste@gmail.com',
   },
   {
     id: 5,
     image: chamika,
-    name: "Chamika Hansamalee",
-    position: "Coordinator - Community Engagement",
-    position2: "and Involement (CEI)",
-    email: "chamikahansamali97@gmail.com",
+    name: 'Chamika Hansamalee',
+    position: 'Coordinator - Community Engagement',
+    position2: 'and Involement (CEI)',
+    email: 'chamikahansamali97@gmail.com',
   },
   {
     id: 6,
     image: hasuni,
-    name: "Hasuni Yasora Withana",
-    position: "Research Associate",
-    email: "hasuni.namaste@gmail.com",
+    name: 'Hasuni Yasora Withana',
+    position: 'Research Associate',
+    email: 'hasuni.namaste@gmail.com',
   },
   {
     id: 7,
     image: heshali,
-    name: "Heshali De Silva",
-    position: "Project Secretary",
-    email: "heshali.namaste@gmail.com",
+    name: 'Heshali De Silva',
+    position: 'Project Secretary',
+    email: 'heshali.namaste@gmail.com',
   },
   {
     id: 8,
     image: poornima,
-    name: "Poornima Madhubhashini",
-    position: "Accountant",
-    email: "finance4namaste@gmail.com",
+    name: 'Poornima Madhubhashini',
+    position: 'Accountant',
+    email: 'finance4namaste@gmail.com',
   },
   // {
   //   id: 9,
@@ -95,46 +99,46 @@ const data = [
   {
     id: 10,
     image: piumi,
-    name: "Piumi Dolawaththa",
-    position: "Research Associate & CST facilitator",
-    email: "piumimahinsala123@gmail.com",
+    name: 'Piumi Dolawaththa',
+    position: 'Research Associate & CST facilitator',
+    email: 'piumimahinsala123@gmail.com',
   },
   {
     id: 11,
     image: rasika,
-    name: "Rasika Haggepola Arachchi",
-    position: "Research Associate",
-    email: "rasikaa.namaste@gmail.com",
+    name: 'Rasika Haggepola Arachchi',
+    position: 'Research Associate',
+    email: 'rasikaa.namaste@gmail.com',
   },
   {
     id: 12,
     image: sakuni,
-    name: "Sakuni Hingurangala",
-    position: "Detection Coordinator",
-    email: "sakunisevwadi@gmail.com",
+    name: 'Sakuni Hingurangala',
+    position: 'Detection Coordinator',
+    email: 'sakunisevwadi@gmail.com',
   },
 
   {
     id: 13,
     image: shashini,
-    name: "Shashini Hewavithanagamage",
-    position: "Intervention Coordinator",
-    email: "shashini.namaste@gmail.com",
+    name: 'Shashini Hewavithanagamage',
+    position: 'Intervention Coordinator',
+    email: 'shashini.namaste@gmail.com',
   },
   {
     id: 14,
     image: Shehara,
-    name: "Shehara Mayadunne",
-    position: "Research Associate",
-    email: "shehara.namaste@gmail.com",
+    name: 'Shehara Mayadunne',
+    position: 'Research Associate',
+    email: 'shehara.namaste@gmail.com',
   },
   {
     id: 15,
     image: yas,
-    name: "R. T. S. Yashodha Rajapaksha",
-    position: "Research Associate & PASS PLUS Master Trainer",
-    position2: "",
-    email: "yashodharajapaksha007@gmail.com",
+    name: 'R. T. S. Yashodha Rajapaksha',
+    position: 'Research Associate & PASS PLUS Master Trainer',
+    position2: '',
+    email: 'yashodharajapaksha007@gmail.com',
   },
   // {
   //   id: 16,
@@ -146,16 +150,44 @@ const data = [
   {
     id: 17,
     image: venura,
-    name: "Venura Rathnayaka",
-    position: "Data Manager",
-    email: "merangavenur@gmail.com",
+    name: 'Venura Rathnayaka',
+    position: 'Data Manager',
+    email: 'merangavenur@gmail.com',
   },
   {
     id: 18,
     image: vidusha,
-    name: "Vidusha Thebuwana",
-    position: "Research Associate",
-    email: "Vidusha.namaste@gmail.com",
+    name: 'Vidusha Thebuwana',
+    position: 'Research Associate',
+    email: 'Vidusha.namaste@gmail.com',
+  },
+  {
+    id: 19,
+    image: jayamini,
+    name: 'Jayamini Lakshika',
+    position: 'Research Associate',
+    email: 'jayamini.namaste@gmail.com',
+  },
+  {
+    id: 20,
+    image: madushalini,
+    name: 'Madushalini Krishnan',
+    position: 'Research Associate',
+    email: 'madu.namaste@gmail.com',
+  },
+  {
+    id: 21,
+    image: anupi,
+    name: 'Anupi Jayawikrama',
+    position: 'Research Associate',
+    email: 'anupi.namaste@gmail.com',
+  },
+  {
+    id: 22,
+    image: kushani,
+    name: 'Kushani Fernando',
+    position: 'Research Associate',
+    email: 'kushani.namaste@gmail.com',
   },
 ];
 
@@ -166,11 +198,11 @@ const SLTeam = () => {
         variant="h4"
         component="h2"
         sx={{
-          fontWeight: "700",
-          fontSize: "64px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          fontWeight: '700',
+          fontSize: '64px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
           mt: 4,
           mb: 3,
         }}
@@ -179,18 +211,18 @@ const SLTeam = () => {
       </Typography>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItem: "center",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItem: 'center',
         }}
       >
         <Grid
           container
           spacing={2}
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItem: "center",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItem: 'center',
             p: 9,
           }}
         >
@@ -203,9 +235,9 @@ const SLTeam = () => {
               sm={6}
               xs={12}
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItem: "center",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItem: 'center',
               }}
             >
               <div className="slteam_card">
@@ -214,8 +246,8 @@ const SLTeam = () => {
                     src={member.image}
                     alt={member.name}
                     style={{
-                      borderRadius: "15px",
-                      transition: "transform 0.3s",
+                      borderRadius: '15px',
+                      transition: 'transform 0.3s',
                     }}
                     width="339px"
                     height="512px"
@@ -224,19 +256,19 @@ const SLTeam = () => {
 
                 <p
                   style={{
-                    fontWeight: "500",
-                    fontSize: "16px",
-                    color: "#474747",
+                    fontWeight: '500',
+                    fontSize: '16px',
+                    color: '#474747',
                   }}
                 >
                   <b>{member.name}</b>
                 </p>
                 <p
                   style={{
-                    fontWeight: "500",
-                    fontSize: "16px",
-                    color: "#474747",
-                    marginBottom: "2px",
+                    fontWeight: '500',
+                    fontSize: '16px',
+                    color: '#474747',
+                    marginBottom: '2px',
                   }}
                 >
                   {member.position}
@@ -245,14 +277,14 @@ const SLTeam = () => {
                 </p>
                 <p
                   style={{
-                    fontSize: "16px",
-                    color: "#474747",
+                    fontSize: '16px',
+                    color: '#474747',
                   }}
                 ></p>
                 <a href={`mailto:${member.email}`}>
                   <IconButton className="sl_IconButton">
                     <Tooltip title={member.email} arrow>
-                      <img src={email} alt="email" />{" "}
+                      <img src={email} alt="email" />{' '}
                     </Tooltip>
                   </IconButton>
                 </a>
--- src/config/Images/Images.js
+++ src/config/Images/Images.js
@@ -1,93 +1,100 @@
-export const namastheLogo = require("../../Common/Assets/Images/NAMASTE_Curves-1 1.png");
-export const namastheLogo2 = require("../../Common/Assets/Images/NAMASTE_Curves-1 2.png");
-export const facebook = require("../../Common/Assets/Images/facebook.png");
-export const instagram = require("../../Common/Assets/Images/instagram.png");
-export const linkedin = require("../../Common/Assets/Images/linkedin.png");
+export const namastheLogo = require('../../Common/Assets/Images/NAMASTE_Curves-1 1.png');
+export const namastheLogo2 = require('../../Common/Assets/Images/NAMASTE_Curves-1 2.png');
+export const facebook = require('../../Common/Assets/Images/facebook.png');
+export const instagram = require('../../Common/Assets/Images/instagram.png');
+export const linkedin = require('../../Common/Assets/Images/linkedin.png');
 
 // Carousel
-export const Carousel1 = require("../../Common/Assets/Images/Property1.png");
-export const Carousel2 = require("../../Common/Assets/Images/3rd picture.png");
-export const Carousel3 = require("../../Common/Assets/Images/Property3.png");
+export const Carousel1 = require('../../Common/Assets/Images/Property1.png');
+export const Carousel2 = require('../../Common/Assets/Images/3rd picture.png');
+export const Carousel3 = require('../../Common/Assets/Images/Property3.png');
 
-export const card1 = require("../../Common/Assets/Images/Copy of About us (6) 4.png");
+export const card1 = require('../../Common/Assets/Images/Copy of About us (6) 4.png');
 
-export const community1 = require("../../Common/Assets/Images/comminuty 1.png");
-export const detection1 = require("../../Common/Assets/Images/detection 1.png");
-export const engagement1 = require("../../Common/Assets/Images/engagement 1.png");
-export const intervention1 = require("../../Common/Assets/Images/intervention 1.png");
+export const community1 = require('../../Common/Assets/Images/comminuty 1.png');
+export const detection1 = require('../../Common/Assets/Images/detection 1.png');
+export const engagement1 = require('../../Common/Assets/Images/engagement 1.png');
+export const intervention1 = require('../../Common/Assets/Images/intervention 1.png');
 
-export const group1 = require("../../Common/Assets/Images/Group.png");
-export const group2 = require("../../Common/Assets/Images/Group2.png");
+export const group1 = require('../../Common/Assets/Images/Group.png');
+export const group2 = require('../../Common/Assets/Images/Group2.png');
 
 // Footer
-export const Rectangle = require("../../Common/Assets/Images/Rectangle__1_-removebg-preview.png");
-export const Rectangle1 = require("../../Common/Assets/Images/Rectangle__3_-removebg-preview.png");
-export const Rectangle2 = require("../../Common/Assets/Images/Rectangle__4_-removebg-preview.png");
-export const Rectangle3 = require("../../Common/Assets/Images/Rectangle__5_-removebg-preview.png");
-export const Rectangle4 = require("../../Common/Assets/Images/Rectangle-removebg-preview.png");
-export const Rectangle5 = require("../../Common/Assets/Images/FC1.png");
-export const Rectangle6 = require("../../Common/Assets/Images/FC2.png");
-export const Rectangle7 = require("../../Common/Assets/Images/FC3.png");
+export const Rectangle = require('../../Common/Assets/Images/Rectangle__1_-removebg-preview.png');
+export const Rectangle1 = require('../../Common/Assets/Images/Rectangle__3_-removebg-preview.png');
+export const Rectangle2 = require('../../Common/Assets/Images/Rectangle__4_-removebg-preview.png');
+export const Rectangle3 = require('../../Common/Assets/Images/Rectangle__5_-removebg-preview.png');
+export const Rectangle4 = require('../../Common/Assets/Images/Rectangle-removebg-preview.png');
+export const Rectangle5 = require('../../Common/Assets/Images/FC1.png');
+export const Rectangle6 = require('../../Common/Assets/Images/FC2.png');
+export const Rectangle7 = require('../../Common/Assets/Images/FC3.png');
 
-export const projectcard1 = require("../../Common/Assets/Images/Clip path group.png");
-export const gif = require("../../Common/Assets/Images/Untitled design.gif");
+export const projectcard1 = require('../../Common/Assets/Images/Clip path group.png');
+export const gif = require('../../Common/Assets/Images/Untitled design.gif');
 
-export const aboutus = require("../../Common/Assets/Images/Copy of About us (17) 1.png");
-export const aboutus1 = require("../../Common/Assets/Images/Copy of About us (17) 2.png");
-export const aboutus2 = require("../../Common/Assets/Images/Copy of About us (17) 6.png");
-export const email = require("../../Common/Assets/Images/email.png");
+export const aboutus = require('../../Common/Assets/Images/Copy of About us (17) 1.png');
+export const aboutus1 = require('../../Common/Assets/Images/Copy of About us (17) 2.png');
+export const aboutus2 = require('../../Common/Assets/Images/Copy of About us (17) 6.png');
+export const email = require('../../Common/Assets/Images/email.png');
 
-export const projectcard2 = require("../../Common/Assets/Images/card.png");
-export const Team1 = require("../../Common/Assets/Images/team1.png");
-export const Team2 = require("../../Common/Assets/Images/team2.png");
-export const underconstruction = require("../../Common/Assets/Images/under-construction-2891888_1280.jpg");
+export const projectcard2 = require('../../Common/Assets/Images/card.png');
+export const Team1 = require('../../Common/Assets/Images/team1.png');
+export const Team2 = require('../../Common/Assets/Images/team2.png');
+export const underconstruction = require('../../Common/Assets/Images/under-construction-2891888_1280.jpg');
 
 // SL Team
-export const asiri = require("../../Common/Assets/Images/asiri.jpg");
-export const raj = require("../../Common/Assets/Images/dr raj.jpg");
-export const chamika = require("../../Common/Assets/Images/chamika.jpg");
-export const hasuni = require("../../Common/Assets/Images/hasuni.jpg");
-export const heshali = require("../../Common/Assets/Images/heshali.jpg");
-export const poornima = require("../../Common/Assets/Images/madu.jpg");
-export const nethma = require("../../Common/Assets/Images/nethma.jpg");
-export const piumi = require("../../Common/Assets/Images/piyumi.jpg");
-export const rasika = require("../../Common/Assets/Images/rasika.jpg");
-export const sakuni = require("../../Common/Assets/Images/sakuni.jpg");
-export const shashini = require("../../Common/Assets/Images/professional photo.jpg");
-export const Shehara = require("../../Common/Assets/Images/shehara.jpg");
-export const yas = require("../../Common/Assets/Images/yashoda.jpg");
-export const abi = require("../../Common/Assets/Images/abi.jpg");
-export const venura = require("../../Common/Assets/Images/venura.jpg");
-export const vidusha = require("../../Common/Assets/Images/vidusha.jpg");
-export const dilini = require("../../Common/Assets/Images/dr dilini.jpg");
-export const ayendre = require("../../Common/Assets/Images/aysha-Untitled design (1).jpg");
+export const asiri = require('../../Common/Assets/Images/asiri.jpg');
+export const raj = require('../../Common/Assets/Images/dr raj.jpg');
+export const chamika = require('../../Common/Assets/Images/chamika.jpg');
+export const hasuni = require('../../Common/Assets/Images/hasuni.jpg');
+export const heshali = require('../../Common/Assets/Images/heshali.jpg');
+export const poornima = require('../../Common/Assets/Images/madu.jpg');
+export const nethma = require('../../Common/Assets/Images/nethma.jpg');
+export const piumi = require('../../Common/Assets/Images/piyumi.jpg');
+export const rasika = require('../../Common/Assets/Images/rasika.jpg');
+export const sakuni = require('../../Common/Assets/Images/sakuni.jpg');
+export const shashini = require('../../Common/Assets/Images/professional photo.jpg');
+export const Shehara = require('../../Common/Assets/Images/shehara.jpg');
+export const yas = require('../../Common/Assets/Images/yashoda.jpg');
+export const abi = require('../../Common/Assets/Images/abi.jpg');
+export const venura = require('../../Common/Assets/Images/venura.jpg');
+export const vidusha = require('../../Common/Assets/Images/vidusha.jpg');
+export const dilini = require('../../Common/Assets/Images/dr dilini.jpg');
+export const ayendre = require('../../Common/Assets/Images/aysha-Untitled design (1).jpg');
+// ======new members==========
+export const jayamini = require('../../Common/Assets/Images/jayamini.png');
+export const madushalini = require('../../Common/Assets/Images/madushalini.png');
+export const anupi = require('../../Common/Assets/Images/anupi.png');
+export const kushani = require('../../Common/Assets/Images/kushani.png');
+//kushani
 
-export const laptopimg = require("../../Common/Assets/Images/About us-1.png");
-export const proobj = require("../../Common/Assets/Images/proobj.png");
-export const withbaby = require("../../Common/Assets/Images/withbaby.png");
 
-export const event1 = require("../../Common/Assets/Images/imag1.JPG");
-export const event2 = require("../../Common/Assets/Images/img2.JPG");
-export const event3 = require("../../Common/Assets/Images/img3.JPG");
-export const event4 = require("../../Common/Assets/Images/event4.jpg");
+export const laptopimg = require('../../Common/Assets/Images/About us-1.png');
+export const proobj = require('../../Common/Assets/Images/proobj.png');
+export const withbaby = require('../../Common/Assets/Images/withbaby.png');
 
-export const family = require("../../Common/Assets/Images/withbaby.png");
-export const contactus = require("../../Common/Assets/Images/contactus.png");
+export const event1 = require('../../Common/Assets/Images/imag1.JPG');
+export const event2 = require('../../Common/Assets/Images/img2.JPG');
+export const event3 = require('../../Common/Assets/Images/img3.JPG');
+export const event4 = require('../../Common/Assets/Images/event4.jpg');
 
-export const card01 = require("../../Common/Assets/Images/card1.png");
-export const card02 = require("../../Common/Assets/Images/croprounded1.jpg");
+export const family = require('../../Common/Assets/Images/withbaby.png');
+export const contactus = require('../../Common/Assets/Images/contactus.png');
 
-export const gt1 = require("../../Common/Assets/Images/gt1.jpg");
-export const gt2 = require("../../Common/Assets/Images/gt2.jpg");
-export const gt3 = require("../../Common/Assets/Images/gt3.jpg");
-export const gt4 = require("../../Common/Assets/Images/gt4.jpg");
-export const gt5 = require("../../Common/Assets/Images/gt5.jpg");
-export const gt6 = require("../../Common/Assets/Images/gt6.jpg");
-export const gt7 = require("../../Common/Assets/Images/gt7.jpg");
-export const gt8 = require("../../Common/Assets/Images/gt8.jpg");
+export const card01 = require('../../Common/Assets/Images/card1.png');
+export const card02 = require('../../Common/Assets/Images/croprounded1.jpg');
 
-export const project2 = require("../../Common/Assets/Images/project2.png");
-export const project3 = require("../../Common/Assets/Images/project3.png");
+export const gt1 = require('../../Common/Assets/Images/gt1.jpg');
+export const gt2 = require('../../Common/Assets/Images/gt2.jpg');
+export const gt3 = require('../../Common/Assets/Images/gt3.jpg');
+export const gt4 = require('../../Common/Assets/Images/gt4.jpg');
+export const gt5 = require('../../Common/Assets/Images/gt5.jpg');
+export const gt6 = require('../../Common/Assets/Images/gt6.jpg');
+export const gt7 = require('../../Common/Assets/Images/gt7.jpg');
+export const gt8 = require('../../Common/Assets/Images/gt8.jpg');
 
-export const project4 = require("../../Common/Assets/Images/Namasthe_Project_-_UI_ss_1_-removebg-preview.png");
-export const project5 = require("../../Common/Assets/Images/Namasthe_Project_s-_UI-removebg-preview.png");
+export const project2 = require('../../Common/Assets/Images/project2.png');
+export const project3 = require('../../Common/Assets/Images/project3.png');
+
+export const project4 = require('../../Common/Assets/Images/Namasthe_Project_-_UI_ss_1_-removebg-preview.png');
+export const project5 = require('../../Common/Assets/Images/Namasthe_Project_s-_UI-removebg-preview.png');
